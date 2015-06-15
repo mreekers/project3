ecb5174ce9dae7ccfc56201c9282cd518076f26d
Michael Reekers <mreekers@Michaels-MacBook-Air-2.local>
2015-06-15 13:43:52 -0700

    heroku

--- project3/db/schema.rb
+++ project3/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150603213305) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "crims", force: :cascade do |t|
     t.string   "statute"
     t.string   "juryinst"
